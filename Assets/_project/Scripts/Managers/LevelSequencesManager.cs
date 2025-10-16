0dc0e288399cc6741b69b8fefb78ecfd7e31376c
Jari Bou <bourdietome@gmail.com>
2025-10-16 15:13:28 +0200

    j'te boulote mon chef @L3ave

--- Assets/_project/Scripts/Managers/LevelSequencesManager.cs
+++ Assets/_project/Scripts/Managers/LevelSequencesManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using _project.ScriptableObjects.Scripts;
+using _project.Scripts.Extensions;
 using _project.Scripts.Managers.Inputs;
 using JetBrains.Annotations;
 using UnityEngine;
@@ -144,9 +145,12 @@ namespace _project.Scripts.Managers
             {
                 if (CurrentSequence.PeakBeat(i, out SequenceData.SequenceAction peakedAction))
                 {
-                    UIManager.Instance.ChangeClothIcon(peakedAction, (int)peakedAction.ClotheColor);
                     UIManager.Instance.ChangeIconPosition(i, peakedAction);
                 }
+            }
+            if (CurrentSequence.GetNextGameAction(out GameAction gameAction))
+            {
+                UIManager.Instance.ChangeClothIcon(gameAction, (int)gameAction.ClotheColor);
             }
         }
 
