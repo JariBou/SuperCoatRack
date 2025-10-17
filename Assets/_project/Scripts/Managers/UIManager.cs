3145bab1838cddaeae348d4b3a018124cfb4cf03
Jari Bou <bourdietome@gmail.com>
2025-10-17 15:31:18 +0200

    yaaay

--- Assets/_project/Scripts/Managers/UIManager.cs
+++ Assets/_project/Scripts/Managers/UIManager.cs
@@ -219,38 +219,50 @@ namespace _project.Scripts.Managers
             Sprite currentSprite = null;
             if (currentGameAction != null)
             {
-                switch (currentGameAction.ClotheType)
+                if (currentGameAction.ActionType == SequenceConfig.ActionType.Bell)
                 {
-                    case SequenceConfig.ClotheType.Coat:
-                        currentSprite = coatToDisplay[(int)currentGameAction.ClotheColor];
-                        break;
-                    case SequenceConfig.ClotheType.Hat:
-                        currentSprite = hatToDisplay[(int)currentGameAction.ClotheColor];
-                        break;
-                    case SequenceConfig.ClotheType.Shoe:
-                        currentSprite = shoesToDisplay[(int)currentGameAction.ClotheColor];
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
+                    currentSprite = iconSprites[(int)SequenceConfig.ActionType.Bell];
+                } else
+                {
+                    switch (currentGameAction.ClotheType)
+                    {
+                        case SequenceConfig.ClotheType.Coat:
+                            currentSprite = coatToDisplay[(int)currentGameAction.ClotheColor];
+                            break;
+                        case SequenceConfig.ClotheType.Hat:
+                            currentSprite = hatToDisplay[(int)currentGameAction.ClotheColor];
+                            break;
+                        case SequenceConfig.ClotheType.Shoe:
+                            currentSprite = shoesToDisplay[(int)currentGameAction.ClotheColor];
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
                 }
             }
             
             Sprite nextSprite = null;
             if (nextGameAction != null)
             {
-                switch (nextGameAction.ClotheType)
+                if (nextGameAction.ActionType == SequenceConfig.ActionType.Bell)
                 {
-                    case SequenceConfig.ClotheType.Coat:
-                        nextSprite = coatToDisplay[(int)nextGameAction.ClotheColor];
-                        break;
-                    case SequenceConfig.ClotheType.Hat:
-                        nextSprite = hatToDisplay[(int)nextGameAction.ClotheColor];
-                        break;
-                    case SequenceConfig.ClotheType.Shoe:
-                        nextSprite = shoesToDisplay[(int)nextGameAction.ClotheColor];
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
+                    nextSprite = iconSprites[(int)SequenceConfig.ActionType.Bell];
+                } else
+                {
+                    switch (nextGameAction.ClotheType)
+                    {
+                        case SequenceConfig.ClotheType.Coat:
+                            nextSprite = coatToDisplay[(int)nextGameAction.ClotheColor];
+                            break;
+                        case SequenceConfig.ClotheType.Hat:
+                            nextSprite = hatToDisplay[(int)nextGameAction.ClotheColor];
+                            break;
+                        case SequenceConfig.ClotheType.Shoe:
+                            nextSprite = shoesToDisplay[(int)nextGameAction.ClotheColor];
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
                 }
             }
             _coatRackPlacementManager.ShowPlacement(currentGameAction);
@@ -260,8 +272,6 @@ namespace _project.Scripts.Managers
         public void InitNextClotheIcon(GameAction currentGameAction, GameAction nextGameAction)
         {
             Sprite currentSprite;
-            
-
             if (currentGameAction.ActionType == SequenceConfig.ActionType.Bell)
             {
                 currentSprite = iconSprites[(int)SequenceConfig.ActionType.Bell];
