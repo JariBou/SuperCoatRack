d7c7239d4aea5c8bbe559f589a2e48247a9e4c7e
Jari Bou <bourdietome@gmail.com>
2025-10-17 15:22:11 +0200

    agugu gaga

--- Assets/_project/Scripts/Managers/UIManager.cs
+++ Assets/_project/Scripts/Managers/UIManager.cs
@@ -260,35 +260,49 @@ namespace _project.Scripts.Managers
         public void InitNextClotheIcon(GameAction currentGameAction, GameAction nextGameAction)
         {
             Sprite currentSprite;
-            switch (currentGameAction.ClotheType)
+            
+
+            if (currentGameAction.ActionType == SequenceConfig.ActionType.Bell)
             {
-                case SequenceConfig.ClotheType.Coat:
-                    currentSprite = coatToDisplay[(int)currentGameAction.ClotheColor];
-                    break;
-                case SequenceConfig.ClotheType.Hat:
-                    currentSprite = hatToDisplay[(int)currentGameAction.ClotheColor];
-                    break;
-                case SequenceConfig.ClotheType.Shoe:
-                    currentSprite = shoesToDisplay[(int)currentGameAction.ClotheColor];
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                currentSprite = iconSprites[(int)SequenceConfig.ActionType.Bell];
+            } else
+            {
+                switch (currentGameAction.ClotheType)
+                {
+                    case SequenceConfig.ClotheType.Coat:
+                        currentSprite = coatToDisplay[(int)currentGameAction.ClotheColor];
+                        break;
+                    case SequenceConfig.ClotheType.Hat:
+                        currentSprite = hatToDisplay[(int)currentGameAction.ClotheColor];
+                        break;
+                    case SequenceConfig.ClotheType.Shoe:
+                        currentSprite = shoesToDisplay[(int)currentGameAction.ClotheColor];
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
             }
             
             Sprite nextSprite;
-            switch (nextGameAction.ClotheType)
+            if (nextGameAction.ActionType == SequenceConfig.ActionType.Bell)
             {
-                case SequenceConfig.ClotheType.Coat:
-                    nextSprite = coatToDisplay[(int)nextGameAction.ClotheColor];
-                    break;
-                case SequenceConfig.ClotheType.Hat:
-                    nextSprite = hatToDisplay[(int)nextGameAction.ClotheColor];
-                    break;
-                case SequenceConfig.ClotheType.Shoe:
-                    nextSprite = shoesToDisplay[(int)nextGameAction.ClotheColor];
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                nextSprite = iconSprites[(int)SequenceConfig.ActionType.Bell];
+            } else
+            {
+                switch (nextGameAction.ClotheType)
+                {
+                    case SequenceConfig.ClotheType.Coat:
+                        nextSprite = coatToDisplay[(int)nextGameAction.ClotheColor];
+                        break;
+                    case SequenceConfig.ClotheType.Hat:
+                        nextSprite = hatToDisplay[(int)nextGameAction.ClotheColor];
+                        break;
+                    case SequenceConfig.ClotheType.Shoe:
+                        nextSprite = shoesToDisplay[(int)nextGameAction.ClotheColor];
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
             }
             NextClotheDisplayManager.InitClotheDisplay(currentSprite, nextSprite);
         }
