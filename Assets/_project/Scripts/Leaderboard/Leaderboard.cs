3786778cfe995bb8863541238f0bb895078eefca
Jari Bou <bourdietome@gmail.com>
2025-10-21 18:53:07 +0200

    Leaderboard saving system

---
+++ Assets/_project/Scripts/Leaderboard/Leaderboard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace _project.Scripts.Leaderboard
+{
+    public class Leaderboard
+    {
+        private static Leaderboard _instance;
+        
+        private Dictionary<string,  List<LeaderboardEntry>> _leaderboard = new();
+
+        private static Leaderboard Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Leaderboard();
+                }
+                return _instance;
+            }
+            set => _instance = value;
+        }
+
+        public static List<LeaderboardEntry> GetLeaderboardForMap(string mapName)
+        {
+            Instance._leaderboard.TryGetValue(mapName, out List<LeaderboardEntry> leaderboard);
+            return leaderboard;
+        }
+
+        public static Dictionary<string, List<LeaderboardEntry>> GetLeaderboard()
+        {
+            return Instance._leaderboard;
+        }
+
+        public static void LoadLeaderboard(Dictionary<string, List<LeaderboardEntry>> leaderboardEntry)
+        {
+            Instance._leaderboard =  leaderboardEntry;
+        }
+    }
+}
---
+++ Assets/_project/Scripts/Leaderboard/LeaderboardEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _project.Scripts.Leaderboard
+{
+    [Serializable]
+    public class LeaderboardEntry
+    {
+        public string Name;
+        public int Score;
+
+        public LeaderboardEntry(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+}
